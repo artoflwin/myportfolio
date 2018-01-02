8a5f3611456e0a60550e463e297bf9cc915573e7
Lwin Htet Aung <artoflwin@gmail.com>
2018-01-02 20:10:20 +0630

    2018 change

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// $("#nav-open").click(function(){
+//   $("#nav-mobile").css({left: 0});
+//   $(this).attr("id","nav-close");
+// });
+// $("#nav-mobile li").click(function(){
+//   $("#nav-mobile").css({right: 89});
+//   $(this).attr("id","nav-open");
+// });
